ce6206334f7203a132af398ed299b80efb008119
rlatap <116437405+RaymartLatap@users.noreply.github.com>
2025-03-30 05:45:33 -0700

    feat: first commit

---
+++ app/(tabs)/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text, View } from "react-native";
+import { BarChart } from 'react-native-gifted-charts';
+
+export default function index() {
+  return (
+    <View>
+      <BarChart/>
+    </View>
+  );
+}
